3018b9f9f761ef0ab50fa4f864be1e69d81cb2e6
backnumberr <chinshi@asagi.waseda.jp>
2024-11-21 12:56:19 +0900

    set MyWebSite file as a regular file

---
+++ backend/MyWebSite/myWeb-pojo/src/test/java/com/example/mywebpojo/MyWebPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywebpojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
