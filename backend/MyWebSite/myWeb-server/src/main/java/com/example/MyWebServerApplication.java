7a714f912e9f59e3fec4988c910bd1ce0459ac44
backnumberr <chinshi@asagi.waseda.jp>
2024-11-21 12:56:19 +0900

    set MyWebSite file as a regular file

---
+++ backend/MyWebSite/myWeb-server/src/main/java/com/example/MyWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebServerApplication.class, args);
+    }
+
+}
---
+++ backend/MyWebSite/myWeb-server/src/test/java/com/example/mywebserver/MyWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
