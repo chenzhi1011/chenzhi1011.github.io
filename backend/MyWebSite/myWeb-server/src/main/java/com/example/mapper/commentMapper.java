34159c115441b4e5e1a58395885f4d191d9f9173
chenzhi <chinshi@asagi.waseda.jp>
2024-11-24 15:09:58 +0900

    build the environment

--- backend/MyWebSite/myWeb-server/src/main/java/com/example/mapper/commentMapper.java
+++ backend/MyWebSite/myWeb-server/src/main/java/com/example/mapper/commentMapper.java
@@ -1,4 +1,11 @@
 package com.example.mapper;
 
+import com.example.entity.Comments;
+import org.apache.ibatis.annotations.*;
+import java.util.List;
+
+@Mapper
 public interface commentMapper {
+    @Select("select * from comments order by create_time desc")
+    List<Comments> getAllCm();
 }
